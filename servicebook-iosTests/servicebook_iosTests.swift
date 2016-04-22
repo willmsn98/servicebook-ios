e007445ebf9e5919077f0cd88343a24bf0549baa
willmsn98 <christopher.e.williamson@gmail.com>
2016-04-21 22:12:14 -0500

    Initial Commit

---
+++ servicebook-iosTests/servicebook_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  servicebook_iosTests.swift
+//  servicebook-iosTests
+//
+//  Created by Christopher Williamson on 4/21/16.
+//  Copyright © 2016 Christopher Williamson. All rights reserved.
+//
+
+import XCTest
+@testable import servicebook_ios
+
+class servicebook_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
